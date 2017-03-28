a6e3931998c17b019b840422c6bf1a100683f0d9
Rohit Koranga <rhtkoranga42@gmail.com>
2017-03-29 03:48:12 +0530

    after resolving polyfill error

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -41,7 +41,7 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/client/shim.min.js
+import 'core-js/client/shim.min.js';
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
